21a71aca3c8734985ec44750a597d4c3c741f1e6
Philip Efem <pefemjnr@gmail.com>
2020-06-09 22:09:19 +0100

    first upload

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   
+    <div class="jumbotron">
+        <h1>File Transfer App</h1>
+        <p class="lead">Simple web application to copy files </p>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:Button ID="Button2" class="btn btn-primary btn-lg" runat="server" Font-Bold="False" Text="Copy" Width="100px" OnClick="Transfer_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="" class="success"></asp:Label>
+        </ContentTemplate>
+        </asp:UpdatePanel>
+        
+    </div>
+    
+    <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+        <ProgressTemplate>
+            copying files please wait...
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/img/prog.gif" />
+        </ProgressTemplate>
+        
+    </asp:UpdateProgress>
+   
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Microsoft.Ajax.Utilities;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Label1.Text = "no files copied";
+
+        }
+
+        public void Transfer_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(5000);
+            Transfer();
+        }
+
+        public void Transfer()
+        {
+            
+            var source = @"C:\#\#\Desktop\WebApplication1\folder1";
+            var destination = @"C:\#\#\Desktop\WebApplication1\folder2";
+            var sourcePath = " ";
+            var targetPath = "";
+            
+
+            var fileNames = new[]
+            {
+                "philip.txt",
+                "viki.txt",
+                "liam.txt"
+            };
+
+            foreach (var fileName in fileNames)
+            {
+                sourcePath = Path.Combine(source, fileName);
+                targetPath = Path.Combine(destination, fileName);
+
+                File.Copy(sourcePath, targetPath, true);
+
+            }
+
+            var fileSource = Directory.GetFiles(@"C:\#\#\Desktop\WebApplication1\folder1");
+            var targetSource = Directory.GetFiles(@"C:\#\#\Desktop\WebApplication1\folder2");
+
+
+            if((fileSource.Length) == (targetSource.Length))
+            {
+                Label1.Text = "All files have been copied";
+            }
+            
+            
+            
+            
+            
+            
+        }
+    }
+}
